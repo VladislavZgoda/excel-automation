2bef6453904331a1ada720b48df6b7b43e2ebacc
VladislavZgoda <www.vlad78@yandex.ru>
2026-08-05 21:03:41 +0300

    feat: implement file saving

--- widgets/matritca_readings_panel.py
+++ widgets/matritca_readings_panel.py
@@ -1,7 +1,12 @@
+import asyncio
+import zipfile
+from datetime import datetime
+from io import BytesIO
 from pathlib import Path
 from typing import cast, get_args
+from zoneinfo import ZoneInfo
 
-from textual import on
+from textual import on, work
 from textual.app import ComposeResult
 from textual.containers import Container
 from textual.reactive import var
@@ -10,15 +15,17 @@ from textual.widgets import (
     Select,
     Static,
 )
+from textual_fspicker import FileSave, Filters
 
 from filters.matritca_readings import BalanceGroupType, filterReadings
-from widgets.file_picker import FilePathSelected, FilePicker
+from widgets.file_picker import FILE_LOCATION, FilePathSelected, FilePicker
 from write_to_excel.matritca_readings import create_wb_reports
 
 
 class MatritcaReadingsPanel(Container):
     readings_path: var[Path | None] = var(None)
     balance_group: var[BalanceGroupType | None] = var(None)
+    wb_reports: var[tuple[BytesIO, BytesIO] | None] = var(None)
 
     def compose(self) -> ComposeResult:
         yield Static("Трансформировать экспорт из Sims в формат для 1С, Приложение №9.")
@@ -31,7 +38,18 @@ class MatritcaReadingsPanel(Container):
             "meter-readings",
         )
         yield Button(
-            "Обработать данные", id="process-data-btn", variant="default", disabled=True
+            "Обработать данные",
+            id="process-data-btn",
+            classes="action-btn",
+            variant="default",
+            disabled=True,
+        )
+        yield Button(
+            "Сохранить отчёты",
+            id="save-file-btn",
+            classes="action-btn",
+            variant="default",
+            disabled=True,
         )
 
     def on_file_path_selected(self, event: FilePathSelected) -> None:
@@ -63,7 +81,32 @@ class MatritcaReadingsPanel(Container):
             )
 
         filtered_readings = filterReadings(self.readings_path, balance_group)
-        wb_reports = create_wb_reports(filtered_readings, balance_group)
+        self.wb_reports = create_wb_reports(filtered_readings, balance_group)
+
+        self._enable_save_file_btn()
+        self.notify("Данные обработаны.", timeout=10)
+
+    @on(Button.Pressed, "#save-file-btn")
+    @work
+    async def handle_save_btn(self) -> None:
+        if self.wb_reports is None:
+            raise ValueError("Требуется объект tuple [BytesIO, BytesIO], получен None.")
+        if save_path := await self.app.push_screen_wait(
+            FileSave(
+                FILE_LOCATION,
+                filters=Filters(("ZIP", lambda p: p.suffix.lower() == ".zip")),
+            ),
+        ):
+            register_buf, supplement_nine_buf = self.wb_reports
+
+            await asyncio.to_thread(
+                self._write_zip,
+                save_path.with_suffix(".zip"),
+                register_buf.getvalue(),
+                supplement_nine_buf.getvalue(),
+            )
+
+            self.notify("Файл сохранён.", timeout=10)
 
     def _check_and_enable_process_data_btn(self) -> None:
         if self.readings_path is None:
@@ -72,3 +115,23 @@ class MatritcaReadingsPanel(Container):
         process_data_btn = self.query_one("#process-data-btn", Button)
         process_data_btn.disabled = False
         process_data_btn.variant = "primary"
+
+    def _enable_save_file_btn(self) -> None:
+        save_file_btn = self.query_one("#save-file-btn", Button)
+        save_file_btn.disabled = False
+        save_file_btn.variant = "success"
+
+    def _write_zip(
+        self,
+        path: Path,
+        register: bytes,
+        supplement: bytes,
+    ) -> None:
+        askue_date = datetime.now(ZoneInfo("Europe/Moscow")).strftime("%d.%m.%Y")
+
+        with (
+            open(path, "wb") as f,
+            zipfile.ZipFile(f, "w", zipfile.ZIP_DEFLATED) as zf,
+        ):
+            zf.writestr(f"АСКУЭ {self.balance_group} {askue_date}.xlsx", register)
+            zf.writestr(f"Приложение №9 {self.balance_group}.xlsx", supplement)
