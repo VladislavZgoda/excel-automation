e35f9f67e68b206a598a37ac28541e9409751440
VladislavZgoda <www.vlad78@yandex.ru>
2026-08-16 13:43:38 +0300

    feat(LegalEntitiesPanel): add event handlers

--- widgets/legal_entities_panel.py
+++ widgets/legal_entities_panel.py
@@ -1,12 +1,21 @@
+from pathlib import Path
+
 from textual.app import ComposeResult
 from textual.containers import Container
+from textual.reactive import var
 from textual.widgets import Button, Static
 
-from widgets.file_picker import FilePicker
-from widgets.folder_picker import FolderPicker
+from widgets.file_picker import FilePathSelected, FilePicker
+from widgets.folder_picker import FolderPathSelected, FolderPicker
 
 
 class LegalEntitiesPanel(Container):
+    sims_readings_path: var[Path | None] = var(None)
+    p2_readings_path: var[Path | None] = var(None)
+    p2_current_readings: var[Path | None] = var(None)
+    template_folder_path: var[Path | None] = var(None)
+    reports_folder_path: var[Path | None] = var(None)
+
     def compose(self) -> ComposeResult:
         yield Static("Перенести данные из экспорта Sims и П2 в отчётные ведомости.")
         yield FilePicker("Выберите файл с Sims показаниями", "sims-readings")
@@ -28,3 +37,23 @@ class LegalEntitiesPanel(Container):
             classes="action-btn",
             disabled=True,
         )
+
+    def on_file_path_selected(self, event: FilePathSelected) -> None:
+        match event.picker_id:
+            case "sims-readings":
+                self.sims_readings_path = event.file_path
+            case "p2-readings":
+                self.p2_readings_path = event.file_path
+            case "p2-current-readings":
+                self.p2_current_readings = event.file_path
+            case _:
+                return
+
+    def on_folder_path_selected(self, event: FolderPathSelected) -> None:
+        match event.picker_id:
+            case "template-folder":
+                self.template_folder_path = event.folder_path
+            case "reports-folder":
+                self.reports_folder_path = event.folder_path
+            case _:
+                return
