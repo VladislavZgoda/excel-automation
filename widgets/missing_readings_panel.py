8cf276f54b0c894e9a266b159c43ba1d2059e14b
VladislavZgoda <www.vlad78@yandex.ru>
2026-09-04 19:48:41 +0300

    feat(MissingReadingsPanel): finish the handle_process_data_btn func

--- widgets/missing_readings_panel.py
+++ widgets/missing_readings_panel.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from textual import on
+from textual import on, work
 from textual.app import ComposeResult
 from textual.containers import Container
 from textual.reactive import var
@@ -40,12 +40,19 @@ class MissingReadingsPanel(Container):
         self._check_and_enable_process_data_btn()
 
     @on(Button.Pressed, "#process-data-btn")
+    @work(thread=True)
     def handle_process_data_btn(self) -> None:
         readings_path = require_not_none(self.readings_path, "readings_path")
         report_path = require_not_none(self.report_path, "report_path")
 
+        self.app.call_from_thread(self._on_process_data_start)
+
         meter_readings = prepare_readings(readings_path, report_path)
         wb = write_readings_report(report_path, meter_readings)
+
+        wb.save(report_path)
+        self.notify("Отсутствующие показания добавлены в отчёт.")
+        self.app.call_from_thread(self._on_process_data_done)
 
     def _check_and_enable_process_data_btn(self) -> None:
         if self.readings_path is None or self.report_path is None:
@@ -54,3 +61,18 @@ class MissingReadingsPanel(Container):
         process_data_btn = self.query_one("#process-data-btn", Button)
         process_data_btn.disabled = False
         process_data_btn.variant = "primary"
+
+    def _on_process_data_start(self) -> None:
+        self.query_one("#process-data-btn", Button).loading = True
+
+    def _on_process_data_done(self) -> None:
+        self.readings_path = None
+        self.report_path = None
+
+        for picker in self.query(FilePicker):
+            picker.reset()
+
+        process_data_btn = self.query_one("#process-data-btn", Button)
+        process_data_btn.loading = False
+        process_data_btn.disabled = True
+        process_data_btn.variant = "default"
